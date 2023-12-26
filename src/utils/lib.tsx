c6f58046f44afd6a9a5746350746f48cb1ddfd0f
Charlytoc <charlyjchaconc@gmail.com>
2023-12-25 23:16:36 -0500

    opacity set to 0 in scrolling animation navbar

--- src/utils/lib.tsx
+++ src/utils/lib.tsx
@@ -1,6 +1,6 @@
 import { Remarkable } from 'remarkable';
 
-export const DEV_MODE = true;
+export const DEV_MODE = false;
 const RIGOBOT_API_URL = "https://rigobot.herokuapp.com";
 
 const fullURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
