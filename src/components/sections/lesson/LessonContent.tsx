59a3c0a0534310f96ab286437cb51ce1a2d64e80
Charlytoc <charlyjchaconc@gmail.com>
2024-01-16 15:22:52 -0500

    fix anchor errors

--- src/components/sections/lesson/LessonContent.tsx
+++ src/components/sections/lesson/LessonContent.tsx
@@ -2,14 +2,39 @@ import useStore from "../../../utils/store";
 import { useEffect } from "react";
 import { convertMarkdownToHTML } from "../../../utils/lib";
 export default function LessonContent() {
-    const { currentContent, start } = useStore(state => ({
+    const { currentContent, start, openLink } = useStore(state => ({
         currentContent: state.currentContent,
-        start: state.start
+        start: state.start,
+        openLink: state.openLink
     }));
 
     useEffect(() => {
         start();
+
     }, [])
+
+    useEffect(() => {
+        const lessonContentDiv = document.querySelector('.lesson-content-component');
+        if (!lessonContentDiv) return;
+
+        const anchors = lessonContentDiv.getElementsByTagName('a');
+    
+        const handleClick = (event:any) => {
+            event.preventDefault();
+            openLink(event.target.href)
+        };
+    
+        for (let anchor of anchors) {
+            anchor.addEventListener('click', handleClick);
+        }
+    
+        // Cleanup event listeners on component unmount
+        return () => {
+            for (let anchor of anchors) {
+                anchor.removeEventListener('click', handleClick);
+            }
+        };
+    }, [currentContent]); // Re-run effect when currentContent changes
 
     return <>
         <div className="lesson-content-component" dangerouslySetInnerHTML={{ __html: convertMarkdownToHTML(currentContent) }}>
