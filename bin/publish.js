f0b2c21fd8168c785167c3c5a51cdeaa5be297ae
Charlytoc <charlyjchaconc@gmail.com>
2024-01-29 21:36:19 -0500

    change publish.js to have the option of increasing the middle version

--- bin/publish.js
+++ bin/publish.js
@@ -1,41 +1,57 @@
+
 const fs = require('fs');
 const archiver = require('archiver');
 const path = require('path');
 
 const rootDir = path.join(__dirname, '..');
 const packageJsonPath = path.join(rootDir, 'package.json');
+
 const packageJson = require(packageJsonPath);
 let version = packageJson.version;
-if(typeof version != "string") throw Error("Invalid package.version found on package.json")
+if (typeof version != "string") throw Error("Invalid package.version found on package.json")
+
 const binPath = __dirname;
 const tarFileName = (v) => `learnpack-${version}.tar.gz`;
 const tarFilePath = path.join(binPath, tarFileName(version));
 
 async function incrementVersion(version, incrementType) {
   const versionParts = version.split('.').map(Number);
+
   if (incrementType === 'major') {
     versionParts[0]++;
     versionParts[1] = 0;
     versionParts[2] = 0;
+  } else if (incrementType === 'middle') {
+    versionParts[1]++;
+    versionParts[2] = 0;
   } else if (incrementType === 'minor') {
     versionParts[2]++;
   }
+
   return versionParts.join('.');
 }
 
 async function checkExistingFile() {
   const inquirer = (await import('inquirer')).default;
   let _v = version;
+
   if (fs.existsSync(tarFilePath)) {
     const answers = await inquirer.prompt([{
       type: 'list',
       name: 'action',
       message: `A file named ${tarFileName(version)} already exists. What would you like to do?`,
-      choices: ['Overwrite', 'Increase Major Version', 'Increase Minor Version']
+      choices: ['Overwrite', 'Increase Major Version', 'Increase Middle Version', 'Increase Minor Version']
     }]);
 
     if (answers.action !== 'Overwrite') {
-      const incrementType = answers.action.includes('Major') ? 'major' : 'minor';
+      let incrementType;
+      if (answers.action.includes('Major')) {
+        incrementType = 'major';
+      } else if (answers.action.includes('Middle')) {
+        incrementType = 'middle';
+      } else {
+        incrementType = 'minor';
+      }
       _v = await incrementVersion(version, incrementType);
       packageJson.version = _v;
       console.log(`Overriding version at package.json at ${packageJsonPath}`)
@@ -45,7 +61,6 @@ async function checkExistingFile() {
 
   version = _v;
   return _v;
-
 }
 
 async function compressDirectory() {
@@ -54,6 +69,7 @@ async function compressDirectory() {
   const _outputPath = path.join(binPath, tarFileName(_v));
   const output = fs.createWriteStream(_outputPath);
   console.log(`Generating ${_outputPath}`);
+
   const archive = archiver('tar', {
     gzip: true,
     zlib: { level: 9 }
