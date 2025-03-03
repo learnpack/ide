8aa6cad27b4c67a6b4ff7d33ec5f5c1d5858f028
Charlytoc <charlyjchaconc@gmail.com>
2025-03-03 12:41:22 -0500

    smaller picture in picture

--- src/components/composites/CustomPictureInPicture/CustomPictureInPicture.tsx
+++ src/components/composites/CustomPictureInPicture/CustomPictureInPicture.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Draggable from "react-draggable";
 import { VideoPlayer } from "../VideoPlayer/VideoPlayer";
 import SimpleButton from "../../mockups/SimpleButton";
@@ -16,6 +16,7 @@ export const CustomPictureInPicture: React.FC<CustomPictureInPictureProps> = ({
   hide,
 }) => {
   const videoContainerRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const { currentExercisePosition } = useStore((s) => ({
     currentExercisePosition: s.currentExercisePosition,
@@ -24,25 +25,28 @@ export const CustomPictureInPicture: React.FC<CustomPictureInPictureProps> = ({
 
   return (
     <>
-      <Draggable cancel=".no-drag">
+      {!isOpen && (
         <div
           ref={videoContainerRef}
           style={{
-            width: "min(500px, 95vw)",
+            width: "min(300px, 95vw)",
             position: "fixed",
             padding: "10px",
             borderRadius: "10px",
-            left: "5vw",
-            top: "20%",
-            transform: "translateX(-50vw) translateY(-50vh)",
+            right: "5vw",
+            bottom: "10%",
             zIndex: 5,
             backgroundColor: "#D8E2F0",
-            cursor: "move",
+            // cursor: "move",
             boxShadow: "0px 0px 10px rgba(0,0,0,0.8)",
           }}
         >
-          <div className="flex-x justify-between">
-            <SimpleButton />
+          <div className="flex-x justify-between align-center">
+            <SimpleButton
+              extraClass="no-drag "
+              action={() => setIsOpen(true)}
+              svg={svgs.expand}
+            />
 
             <span className="padding-small">
               {currentExercisePosition === 0
@@ -57,7 +61,46 @@ export const CustomPictureInPicture: React.FC<CustomPictureInPictureProps> = ({
           </div>
           <VideoPlayer link={link} />
         </div>
-      </Draggable>
+      )}
+      {isOpen && (
+        <Draggable cancel=".no-drag">
+          <div
+            ref={videoContainerRef}
+            style={{
+              width: "min(600px, 95vw)",
+              position: "fixed",
+              padding: "10px",
+              borderRadius: "10px",
+              left: "5vw",
+              top: "10%",
+              zIndex: 5,
+              backgroundColor: "#D8E2F0",
+              cursor: "move",
+              boxShadow: "0px 0px 10px rgba(0,0,0,0.8)",
+            }}
+          >
+            <div className="flex-x justify-between align-center">
+              <SimpleButton
+                extraClass="no-drag"
+                action={() => setIsOpen(false)}
+                svg={svgs.reduce}
+              />
+
+              <span className="padding-small">
+                {currentExercisePosition === 0
+                  ? t("video-introduction")
+                  : t("video-solution")}
+              </span>
+              <SimpleButton
+                extraClass="no-drag"
+                action={hide}
+                svg={svgs.redClose}
+              />
+            </div>
+            <VideoPlayer link={link} />
+          </div>
+        </Draggable>
+      )}
     </>
   );
 };
