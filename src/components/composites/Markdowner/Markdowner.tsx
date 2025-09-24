9c504f95b2d850bf55903429349b0b3fa0748ec7
Charlytoc <charlyjchaconc@gmail.com>
2025-09-24 12:53:41 -0500

    Update components info

--- src/components/composites/Markdowner/Markdowner.tsx
+++ src/components/composites/Markdowner/Markdowner.tsx
@@ -467,6 +467,8 @@ const CustomCodeBlock = ({
   const { t } = useTranslation();
   const [executionResult, setExecutionResult] = useState<string | null>(null);
 
+  console.log(language, "language");
+
   if (language === "stdout" || language === "stderr") {
     return (
       <div className={`${language}`}>
