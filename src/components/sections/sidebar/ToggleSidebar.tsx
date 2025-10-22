80a6aaf0e3b45f7efb16d2aca21249ac661f21d6
mfagundez <mfagundez@4geeks.com>
2025-10-22 15:05:11 -0300

    feat: Add a tooltip to the ToggleSidebar hamburger button

--- src/components/sections/sidebar/ToggleSidebar.tsx
+++ src/components/sections/sidebar/ToggleSidebar.tsx
@@ -1,8 +1,10 @@
 import { svgs } from "../../../assets/svgs";
 import useStore from "../../../utils/store";
 import SimpleButton from "../../mockups/SimpleButton";
+import { useTranslation } from "react-i18next";
 
 export const ToggleSidebar = () => {
+  const { t } = useTranslation();
   const {
     setShowSidebar,
     showSidebar,
@@ -19,6 +21,7 @@ export const ToggleSidebar = () => {
     <SimpleButton
       svg={svgs.dropdownButton}
       id="sidebar-toggle"
+      title={t("toggle-sidebar")}
       extraClass={`padding-small rounded ${showSidebar ? "bg-rigo " : ""}`}
       action={() => {
         setShowSidebar(!showSidebar);
